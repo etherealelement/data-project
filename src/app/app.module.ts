c4324f6e44ee6f8bc5fb1123743a93bb677874de
vmeset <luzin.itrum@gmail.com>
2023-10-04 10:51:07 +0300

    init: project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TextfieldComponent } from './components/textfield/textfield.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TextfieldComponent
   ],
   imports: [
     BrowserModule,
